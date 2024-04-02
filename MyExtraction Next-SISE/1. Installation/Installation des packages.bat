bf0a42d2e71f6293943fe788c8d6c540d2dc74a0
Corentin PRADIE <106449070+CorentinPRADIE@users.noreply.github.com>
2024-04-02 16:33:42 +0200

    Delete MyExtraction Next-SISE directory

--- MyExtraction Next-SISE/1. Installation/Installation des packages.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Ajoute les chemins au PATH de l'utilisateur actuel de manière permanente
-setx PATH "%PATH%;C:\Users\ADSL\Desktop\Python;C:\Users\ADSL\Desktop\Python\Scripts"
-
-echo Installation des packages Python...
-pip install pandas streamlit dask
-echo Les chemins ont été ajoutés au PATH de l'utilisateur et les packages ont été installés.
-echo Fin de l'installation. Appuyez sur une touche pour fermer.
-pause > nul
